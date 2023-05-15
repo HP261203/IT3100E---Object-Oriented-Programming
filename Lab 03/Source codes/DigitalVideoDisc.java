c394b821d51de7005d92ae4f7b8e772af8ae6afd
Phạm Quang Huy <130724254+HP261203@users.noreply.github.com>
2023-05-15 16:43:13 +0700

    Add files via upload

---
+++ Lab 03/Source codes/DigitalVideoDisc.java
@@ -0,0 +1,95 @@
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String directory;
+    private int length;
+    private float cost;
+    private static int nbDigitalVideoDiscs = 0;
+    private int id = 0;
+
+    public DigitalVideoDisc(String title){
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost){
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+        this.nbDigitalVideoDiscs ++;
+        this.id = this.nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String directory, String category, String title, float cost){
+        this.directory = directory;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+        this.nbDigitalVideoDiscs ++;
+        this.id = this.nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String directory, int length, float cost){
+        this.title = title;
+        this.category = category;
+        this.directory = directory;
+        this.length = length;
+        this.cost = cost;
+        this.nbDigitalVideoDiscs ++;
+        this.id = this.nbDigitalVideoDiscs;
+    }
+
+    public String getTitle(){
+        return this.title;
+    } 
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    public String getDirectory(){
+        return this.directory;
+    }
+
+    public int getLength(){
+        return this.length;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public float getCost(){
+        return this.cost;
+    }
+
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static void main(String[] args) {
+        DigitalVideoDisc dvd = new DigitalVideoDisc("Anime","Doraemon",3.84f);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Animation","Jujutsu Kaisen",5.99f);
+        System.out.println(dvd.getId());
+    }
+}
