c3edb3be6e0a27e0d1acbade8edb4641568c9c0d
Phạm Quang Huy <130724254+HP261203@users.noreply.github.com>
2023-06-11 23:56:31 +0700

    Delete Lab 04/Code directory

--- Lab 04/Code/MediaComparatorByTitle.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class MediaComparatorByTitle implements Comparator<Media>{
-    @Override
-    public int compare(Media o1, Media o2) {
-        return o1.getTitle().compareTo(o2.getTitle());
-    }
-}
